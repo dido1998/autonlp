abc2c41175c4a2e8042c3a20931abcf32388fc5b
aniket <adidolkar123@gmail.com>
2018-07-13 23:17:20 +0530

    adding support for text generation

---
+++ generation/tensorflow_generate.py
@@ -0,0 +1,189 @@
+import tensorflow as tf
+from pathlib import Path
+import sys
+import numpy as np
+import os
+from tqdm import tqdm
+class basic_rnn_model():
+	def __init__(self,num_layers,lr,rnn_block,embed_matrix1,end_token,num_units,fc1,fc2,vocab_size,device):
+		
+		self.inputs=tf.placeholder(tf.int32,shape=[None,None],name='inputs')#tensor of shape [batch_size,sequence_len] 
+		self.sq=tf.placeholder(tf.int32,shape=[None],name='sequence_length')#sequence leangth for each instance
+		with tf.name_scope('embed'):
+			with tf.device(device):
+				self.W = tf.get_variable(name = 'W', shape = embed_matrix1.shape, initializer = tf.constant_initializer(embed_matrix1), trainable = True)#embed_matrix1 is the pretrained glove embeddings
+				self.embeddings_out = tf.nn.embedding_lookup(self.W, self.inputs)
+
+		
+		self.keep_prob=tf.placeholder(tf.float32,name='keep_probabaility')#dropout probobality
+		self.labels=tf.placeholder(tf.int32,shape=[None,None])#tensor of shape [batch_size,sequence_length]
+		self.starttoken=tf.placeholder(tf.int32,shape=[None])#index of the start token in the vocabulary 
+		self.endtoken=tf.constant(end_token)#index of the end token in the vocabulary
+		self.weights=tf.placeholder(tf.float32,shape=[None,None])
+
+		
+		self.train_helper = tf.contrib.seq2seq.TrainingHelper(self.embeddings_out, self.sq)
+
+		self.pred_helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(self.W, start_tokens=self.starttoken, end_token=self.endtoken)
+
+		def decode(helper, scope,batch_size,reuse=None):
+			with tf.variable_scope(scope, reuse=reuse):
+				cell=None
+				if rnn_block is 'LSTM':
+					cell =[tf.contrib.rnn.DropoutWrapper(tf.contrib.rnn.BasicLSTMCell (size,activation=tf.nn.relu),state_keep_prob=self.keep_prob) for size in [num_units]*num_layers]  
+				else:
+					cell =[tf.contrib.rnn.DropoutWrapper(tf.contrib.rnn.GRUCell (size,activation=tf.nn.relu),state_keep_prob=self.keep_prob) for size in [num_units]*num_layers]
+				cell = tf.nn.rnn_cell.MultiRNNCell(cell, state_is_tuple=True)
+				out_cell = tf.contrib.rnn.DropoutWrapper(tf.contrib.rnn.OutputProjectionWrapper(
+				        cell,fc1,activation=tf.nn.relu,reuse=reuse),output_keep_prob=self.keep_prob)
+				out_cell =  tf.contrib.rnn.DropoutWrapper(tf.contrib.rnn.OutputProjectionWrapper(
+				        out_cell,fc2,activation=tf.nn.relu,reuse=reuse),output_keep_prob=self.keep_prob)
+				out_cell = tf.contrib.rnn.OutputProjectionWrapper(out_cell,vocab_size,activation=None,reuse=reuse)
+				decoder = tf.contrib.seq2seq.BasicDecoder(
+				    cell=out_cell, helper=helper,
+				    initial_state=out_cell.zero_state(dtype=tf.float32, batch_size=batch_size))
+				outputs = tf.contrib.seq2seq.dynamic_decode(
+				    decoder=decoder,impute_finished=True, output_time_major=False
+				     )
+				return outputs[0]
+		self.train_outputs = decode(self.train_helper, 'decode',tf.shape(self.inputs)[0])
+		self.pred_outputs = decode(self.pred_helper, 'decode',tf.shape(self.starttoken)[0],reuse=True)
+		self.train_outputs=self.train_outputs[0]# train_outputs is tensor of shape [batch_size,sequence_length,vocab_size]
+
+		self.pred_outputs=self.pred_outputs[0]#pred_outputs is tensor of shape [batch_size,sequence_length,vocab_size]
+		self.loss=tf.contrib.seq2seq.sequence_loss(logits=self.train_outputs,targets=self.labels,weights=self.weights)
+		
+		self.optimize=tf.train.AdamOptimizer(lr)
+		self.grads=self.optimize.compute_gradients(self.loss)
+		self.opt=self.optimize.apply_gradients(self.grads)
+
+		self.saver=tf.train.Saver()
+		self.graph=tf.get_default_graph()
+
+def read_glove_vecs(glove_file):
+    with open(glove_file, 'r') as f:
+        words = set()
+        word_to_vec_map = {}
+        for line in f:
+            line = line.strip().split()
+            curr_word = line[0]
+            words.add(curr_word)
+            word_to_vec_map[curr_word] = np.array(line[1:], dtype=np.float64)
+        
+        i = 1
+        words_to_index = {}
+        
+        index_to_words = {}
+        for w in sorted(words):
+            words_to_index[w] = i
+            index_to_words[i] = w
+            i = i + 1
+    return words_to_index, index_to_words, word_to_vec_map
+
+def run(data,numepochs,save_model_after_n_epochs,num_layers,lr,rnn_block,num_units,fc1,fc2,vocab_size,model_save_dir,dictionary,num_training_examples,device,keep_prob=0.5,minibatch_size=32,testduringtrain=True,restore=False):
+	#execute the following two lines to get the glove embeddings
+	#wget http://nlp.stanford.edu/data/glove.6B.zip 
+	#unzip glove.6B.zip -d content
+	words_to_index, index_to_words, word_to_vec_map = read_glove_vecs('generation/glove.txt')
+	embed_matrix1 = np.zeros((vocab_size, 300))#contains glove vectors for each word in vocabulary
+	for word,index in dictionary.items():
+		try:
+			embed_matrix1[index, :] = word_to_vec_map[word]
+		except:
+			embed_matrix1[index, :] = np.random.uniform(-1, 1, 300)
+	endtoken=dictionary['<end>']
+	meta_file_path='generation/meta.json'
+	meta_file=None
+	json_data=None
+	if os.path.exists(meta_file_path):
+		if restore==False:
+			meta_file=open(meta_file_path,'w')
+		else:
+			meta_file=open(meta_file_path,'r')
+			json_data=json.load(meta_file)
+			meta_file.close()
+			meta_file=open(meta_file_path,'w')
+	else:
+		if restore==True:
+			print('no saved model found')
+			print('exiting...')
+			sys.exit()
+		else:
+			meta_file=open(meta_file_path,'w')
+	
+	latest_model_path=None
+	start_epoch_num=0
+	if restore==True:
+		start_epoch_num=json['num_epochs_over']
+		latest_model_path=json['model_path']
+
+	model=basic_rnn_model(num_layers,lr,rnn_block,embed_matrix1,endtoken,num_units,fc1,fc2,vocab_size,device)
+	
+	with tf.Session(graph=model.graph) as sess:
+		if restore==True:
+			saver.restore(sess,latest_model_path)
+			print('model '+str(latest_model_path)+' restored')
+
+		else:
+			sess.run(tf.global_variables_initializer())
+			print('new model started')
+		print('entering training loop')
+		for k in range(start_epoch_num,numepochs):
+			l=0
+			for i in tqdm(range(int(num_training_examples/minibatch_size))):
+				cur_ip=data[i*minibatch_size:i*minibatch_size+minibatch_size]
+				max_seq_len_in_curr_batch=0
+				for c in cur_ip:
+					h11=c.split()
+					if len(h11)>max_seq_len_in_curr_batch:
+						max_seq_len_in_curr_batch=len(h11)
+				lbs=np.zeros([minibatch_size,max_seq_len_in_curr_batch+1])#array to store labels
+				inps=np.zeros([minibatch_size,max_seq_len_in_curr_batch+1])#array to store inputs
+				ws=np.zeros([minibatch_size,max_seq_len_in_curr_batch+1])#array to store weights
+				for x in range(minibatch_size):
+					inps[x,0]=dictionary['<start>']
+					ws[x,0]=1
+				c1=0
+				sqlen=np.zeros([minibatch_size])
+				for c in cur_ip:
+					h=c.split()
+					c2=1
+					c3=0
+					sqlen[c1]=len(h)+1
+					for j in h:
+						ws[c1,c2]=1
+				
+						lbs[c1,c3]=dictionary[j]
+						inps[c1,c2]=dictionary[j]
+						c2+=1
+						c3+=1
+					c1+=1
+				loss,_=sess.run([model.loss,model.opt],feed_dict={model.sq:sqlen,model.keep_prob:keep_prob,model.weights:ws,model.starttoken:np.array([dictionary['<start>']]),model.inputs:inps,model.labels:lbs})
+				l+=loss
+
+			if testduringtrain==True:
+				f=sess.run(model.pred_outputs,feed_dict={model.starttoken:np.array([dictionary['<start>']]),model.keep_prob:1.0})
+				a1=f[0,:,:]
+				
+				preds=''
+				for i in range(a1.shape[0]):
+					w1=np.argmax(a1[i,:])	
+					for q,t in dictionary.items():
+						if t==w1:
+							preds=preds+" "+q
+				print(preds)
+			cur_ip=[]
+
+			l=l/minibatch_size
+
+			print(str(k)+':loss:'+str(l))
+			if k%save_model_after_n_epochs==0:
+				try:
+
+					os.mkdir(model_save_dir+'/genation_model_'+str(k)+'/gen_model')
+				except:
+					pass
+				saver.save(sess,model_save_dir+'/genation_model_'+str(k)+'/gen_model')
+			json_data['model_path']=model_save_dir+'/genation_model_'+str(k)+'/gen_model'
+			json_data['num_epochs_over']=k
+			json.dump(json_data,meta_file)
---
+++ main.py
@@ -0,0 +1,126 @@
+from tensorflow import flags
+import nltk
+import re
+import numpy as np
+import generation.tensorflow_generate as runner
+import os, shutil
+
+Flags=flags.FLAGS
+
+
+class Generation():
+	def clean_str(self,string):
+		string = string.strip().lower()
+
+		string = re.sub(r"\s{2,}", " ", string)
+		string = re.sub(r',', '', string)
+		string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+		if 'let\'s' in string:
+		    string = re.sub(r'let\'s', 'let us', string)
+		if 'lets' in string:
+		    string = re.sub(r'lets', 'let us', string)
+
+		string = re.sub(r"\'s", " is", string)
+		string = re.sub(r"\'ve", " have", string)
+		if 'wont ' in string:
+		    string = re.sub(r"won\'?t", "will not", string)
+		if 'won\'t ' in string:
+		    string = re.sub(r"won\'?t", "will not", string)
+
+		if 'cant ' in string:
+		    string = re.sub(r"n\'?t", " can not", string)
+		if 'can\'t ' in string:
+		    string = re.sub(r"n\'?t", " can not", string)
+
+		string = re.sub(r"n\'t", " not", string)
+		string = re.sub(r"\'re", " are", string)
+		string = re.sub(r"\'d", " \'d", string)
+		string = re.sub(r"\'ll", " will", string)
+		string = re.sub(r",", " , ", string)
+		string = re.sub(r"!", "", string)
+		string = re.sub(r"\(", " \( ", string)
+		string = re.sub(r"\)", " \) ", string)
+		string = re.sub(r"\?", "", string)
+		string = re.sub(r"\s{2,}", " ", string)
+		# string = re.sub(r"\'", '', string)
+
+		return string.strip()
+	def preprocess(self,sentence):
+		sentence = sentence.lower()
+		sentence=self.clean_str(sentence)
+
+		tokenizer = nltk.RegexpTokenizer(r'\w+')
+
+		tokens = tokenizer.tokenize(sentence)
+		m=len(tokens)
+		tokens=" ".join(tokens)
+		return m,tokens
+	def get_data(self):
+		#this funtion assumes that data is present in a text file with each instance on seperate line
+		#this function can be altered as required to get data from different files.
+		#it returns the sentences as a list and the dictionary
+		f=open(Flags.datafile,'r')
+		text=f.read();
+
+		sentence=text.split('\n')
+		processed_sent=[]
+
+
+		max=0
+		for s in sentence:
+			m,sent=self.preprocess(s)
+			if m>max:
+				max=m
+			processed_sent.append(sent)
+		dictionary={}
+		dictionary['<start>']=0
+		dictionary['<end>']=1
+		for p in processed_sent:
+			words=p.split()
+			for w in words:
+				if w not in dictionary:
+					dictionary[w]=len(dictionary)
+		return processed_sent,dictionary
+	def run(self):
+		data,dictionary=self.get_data()
+		print('got data from '+str(Flags.datafile))
+
+		try:
+			os.mkdir(Flags.model_save_path)
+			print('making directory '+str(Flags.model_save_path)+' to save models')
+		except:
+			print('saving model to '+str(Flags.model_save_path)+' directory')
+			if Flags.restore==False:
+				folder = Flags.model_save_path
+				for the_file in os.listdir(folder):
+					file_path = os.path.join(folder, the_file)
+					try:
+						if os.path.isfile(file_path):
+						    os.unlink(file_path)
+						elif os.path.isdir(file_path): shutil.rmtree(file_path)
+					except Exception as e:
+						pass
+		 
+		runner.run(data,Flags.num_epochs,Flags.save_model_after_n_epochs,Flags.num_rnn_layers,Flags.learning_rate,Flags.rnn_block,Flags.num_units,Flags.fc1,Flags.fc2,len(dictionary),Flags.model_save_path,dictionary,len(data),Flags.device)
+
+
+
+if __name__=='__main__':
+	flags.DEFINE_string("datafile",'generation/image_coco.txt','file which has the data')
+	flags.DEFINE_string("task",'generation','currently on language generation is supported')
+	flags.DEFINE_integer("num_epochs",10,'number of epochs')
+	flags.DEFINE_integer('save_model_after_n_epochs',2,'number of epochs after which to save model')
+	#generation specific parameters
+	flags.DEFINE_string("rnn_block",'LSTM','can be GRU or LSTM')
+	flags.DEFINE_integer('num_units',32,'number of units in each lstm layer')
+	flags.DEFINE_string("architecture of the block",'','can be uni-directional or bi-directional')
+	flags.DEFINE_integer("num_rnn_layers",2,"number of layers in the recurrent neural network")
+	flags.DEFINE_integer("fc1",128,'num units in first fc layer')
+	flags.DEFINE_integer("fc2",256,'num units in second fc layer')
+	flags.DEFINE_float('learning_rate',5e-4,'Learning rate for generation')
+	flags.DEFINE_string('model_save_path','generation/model','directory to save model')
+	flags.DEFINE_bool('testduringtrain',True,'enable testing during training')
+	flags.DEFINE_bool('restore_model',False,'True for restore model , False for starting new model')
+	flags.DEFINE_string('device','/device:CPU:0','device to train the model on')
+	g=Generation()
+	g.run()
